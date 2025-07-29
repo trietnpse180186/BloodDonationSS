e0b4898fc6a39e471b4d5d6bd1938bafb36a6039
Ho Dinh Anh <anhhdse180670@fpt.edu.vn>
2025-07-30 01:26:15 +0700

    update Status notification

--- BE/BloodDonation/src/main/java/com/swpproject/BloodDonation/dto/request/VerifyOtpRequest.java
+++ BE/BloodDonation/src/main/java/com/swpproject/BloodDonation/dto/request/VerifyOtpRequest.java
@@ -1,3 +1,4 @@
+package com.swpproject.BloodDonation.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
